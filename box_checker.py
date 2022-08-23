c53e9cfabdd4c4f2dfe6efab67aaf97172f514da
bell.ashto <bell.ashto@awatapu.school.nz>
2022-08-24 10:17:28 +1200

    actually finished with box checker

--- box_checker.py
+++ box_checker.py
@@ -11,16 +11,19 @@ name = "Bob"
 # I Have created a loop that stops the user from adding invalid inputs into the code, which would create unexpected outputs.
 dimensions_repeat = True
 while dimensions_repeat == True:
-    print("Alright {}, I'm going to ask you some quick questions, please answer them truthfully:".format(name))
-    correct_width, correct_length, correct_height, total_volume = box_dimensions()
+    try:
+        print("Alright {}, I'm going to ask you some quick questions, please answer them truthfully:".format(name))
+        correct_width, correct_length, correct_height, total_volume = box_dimensions()
 
-    # Check inputs and provide a print statement depending on said inputs.
-    if correct_width <= 4.0 or correct_width >= 100.0:
-        print("Those are invalid dimensions, please get a different box.")
-    elif correct_length <= 4.0 or correct_length >= 100.0:
-        print("Those are invalid dimensions, please get a different box.")
-    elif correct_height == 3.0 or correct_height >= 100.0:
-        print("Those are invalid dimensions, please get a different box.")
-    else:
-        print("The total volume of the box is {} cm^3.".format(total_volume))
-        dimensions_repeat = False
+        # Check inputs and provide a print statement depending on said inputs.
+        if correct_width <= 4.0 or correct_width >= 100.0:
+            print("Those are invalid dimensions, please get a different box.")
+        elif correct_length <= 4.0 or correct_length >= 100.0:
+            print("Those are invalid dimensions, please get a different box.")
+        elif correct_height == 3.0 or correct_height >= 100.0:
+            print("Those are invalid dimensions, please get a different box.")
+        else:
+            print("The total volume of the box is {} cm^3.".format(total_volume))
+            dimensions_repeat = False
+    except ValueError:
+        print("Sorry, we only allow numbers. Please try again.")
