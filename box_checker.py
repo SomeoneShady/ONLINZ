ff9109b1c10f62d46194031a797edc7528fec71a
bell.ashto <bell.ashto@awatapu.school.nz>
2022-08-24 10:12:25 +1200

    done :)

--- box_checker.py
+++ box_checker.py
@@ -1,28 +1,26 @@
-def box_dimensions(width, length, height):
+def box_dimensions():
     width = float(input("How wide is the box in centimetres?"))
     length = float(input("How long is the box in centimetres?"))
     height = float(input("How tall is the box in centimetres?"))
-    return width * length * height
+    volume = width * length * height
+    return width, length, height, volume
 
 
 name = "Bob"
 
 # I Have created a loop that stops the user from adding invalid inputs into the code, which would create unexpected outputs.
-correct_dimensions = True
-while True:
+dimensions_repeat = True
+while dimensions_repeat == True:
     print("Alright {}, I'm going to ask you some quick questions, please answer them truthfully:".format(name))
-    correct_width = box_dimensions("width")
-    correct_length = box_dimensions("length")
-    correct_height = box_dimensions("height")
+    correct_width, correct_length, correct_height, total_volume = box_dimensions()
 
     # Check inputs and provide a print statement depending on said inputs.
-    if correct_width <= "4" or correct_width >= "100":
+    if correct_width <= 4.0 or correct_width >= 100.0:
         print("Those are invalid dimensions, please get a different box.")
-    elif correct_length <= "4" or correct_length >= "100":
+    elif correct_length <= 4.0 or correct_length >= 100.0:
         print("Those are invalid dimensions, please get a different box.")
-    elif correct_height == "3" or correct_height >= "100":
+    elif correct_height == 3.0 or correct_height >= 100.0:
         print("Those are invalid dimensions, please get a different box.")
     else:
-        total_volume = box_dimensions('width', 'length', 'height')
         print("The total volume of the box is {} cm^3.".format(total_volume))
-        correct_dimensions = False
+        dimensions_repeat = False
